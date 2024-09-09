5203c16766a93180cbe67c8f7ce750d0318013e3
Kevin <kzhe0012@student.monash.edu>
2024-09-09 16:16:18 +1000

    Revert "Simplify log syntax"
    This reverts commit 76869ca2731f37da4d1e17235cf02539c18df320.

--- lib_piglet/search/dijkstra_search.py
+++ lib_piglet/search/dijkstra_search.py
@@ -28,15 +28,13 @@ class dijkstra_search(base_search):
         self.max_depth_ = 0
         self.goal_ = target_state
         self.start_time = time.process_time()
-        start_node = self.generate(start_node, None, None)
-        self.log("source", start_node)
+        start_node = self.log("source", self.generate(start_node, None, None))
         self.open_list_.push(start_node)
         self.all_nodes_list_[start_node] = start_node
 
         # continue while there are still nods on OPEN
         while len(self.open_list_) > 0:
-            current = self.open_list_.pop()
-            self.log("close", current)
+            current = self.log("close", self.open_list_.pop())
             current.close()
             self.nodes_expanded_ += 1
             if current.depth_ > self.max_depth_:
@@ -57,8 +55,7 @@ class dijkstra_search(base_search):
                 # each successor is a (state, action) tuple which
                 # which we map to a corresponding search_node and push
                 # then push onto the OPEN list
-                succ_node = self.generate(state, action, current)
-                self.log("expand", succ_node)
+                succ_node = self.log("expand", self.generate(state, action, current))
                 # succ_node not in any list, add it to open list
                 if succ_node not in self.all_nodes_list_:
                     # we need this open_handle_ to update the node in open list in the future
--- lib_piglet/search/graph_search.py
+++ lib_piglet/search/graph_search.py
@@ -28,16 +28,14 @@ class graph_search(base_search):
         self.start_ = start_state
         self.goal_ = goal_state
         self.start_time = time.process_time()
-        start_node = self.generate(start_state, None, None)
-        self.log("source", start_node)
+        start_node = self.log("source", self.generate(start_state, None, None))
         self.log("destination", self.generate(goal_state, None, None))
         self.open_list_.push(start_node)
         self.all_nodes_list_[start_node] = start_node
 
         # continue while there are still nods on OPEN
         while len(self.open_list_) > 0:
-            current = self.open_list_.pop()
-            self.log("close", current)
+            current = self.log("close", self.open_list_.pop())
             current.close()
             self.nodes_expanded_ += 1
 
@@ -61,8 +59,7 @@ class graph_search(base_search):
                 # each successor is a (state, action) tuple which
                 # which we map to a corresponding search_node and push
                 # then push onto the OPEN list
-                succ_node = self.generate(state, action, current)
-                self.log("generate", succ_node)
+                succ_node = self.log("generate", self.generate(state, action, current))
                 # succ_node not in any list, add it to open list
                 if succ_node not in self.all_nodes_list_:
                     self.log("keep", succ_node)
